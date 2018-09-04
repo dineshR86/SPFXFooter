6440b589b0e90c3a0d099d322eb7cc89b595e905
dineshR86 <37539383+dineshR86@users.noreply.github.com>
2018-09-04 20:10:28 +0530

    Uploaded the initial files to the GIT

---
+++ src/extensions/spfxExtension1/SpfxExtension1ApplicationCustomizer.ts
@@ -0,0 +1,69 @@
+import { override } from '@microsoft/decorators';
+import { Log } from '@microsoft/sp-core-library';
+import styles from './AppCustomizer.module.scss';
+import {
+  BaseApplicationCustomizer,
+  PlaceholderContent,
+  PlaceholderName
+} from '@microsoft/sp-application-base';
+import { Dialog } from '@microsoft/sp-dialog';
+import * as strings from 'SpfxExtension1ApplicationCustomizerStrings';
+import {escape} from '@microsoft/sp-lodash-subset';
+const LOG_SOURCE: string = 'SpfxExtension1ApplicationCustomizer';
+
+/**
+ * If your command set uses the ClientSideComponentProperties JSON input,
+ * it will be deserialized into the BaseExtension.properties object.
+ * You can define an interface to describe it.
+ */
+export interface ISpfxExtension1ApplicationCustomizerProperties {
+  // This is an example; replace with your own property
+  testMessage: string;
+}
+
+/** A Custom Action which can be run during execution of a Client Side Application */
+export default class SpfxExtension1ApplicationCustomizer
+  extends BaseApplicationCustomizer<ISpfxExtension1ApplicationCustomizerProperties> {
+
+  private _topPlaceholder:PlaceholderContent|undefined;
+  private _bottomPlaceholder:PlaceholderContent|undefined;
+  
+    @override
+  public onInit(): Promise<void> {
+    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+
+    this.context.placeholderProvider.changedEvent.add(this,this._renderPlaceHolders);
+
+    this._renderPlaceHolders();
+
+    return Promise.resolve();
+  }
+
+  private _renderPlaceHolders():void{
+    console.log('HelloWorldApplicationCustomizer._renderPlaceHolders()');
+    console.log('Available placeholders: ',this.context.placeholderProvider.placeholderNames.map(name => PlaceholderName[name]).join(', '));
+
+    if(!this._bottomPlaceholder){
+      this._bottomPlaceholder=this.context.placeholderProvider.tryCreateContent(
+        PlaceholderName.Bottom,{onDispose:this._onDispose}
+      );
+
+      if(!this._bottomPlaceholder){
+        console.error('the expected placeholder bottom was not found');
+        return;
+      }
+
+      if(this._bottomPlaceholder.domElement){
+      this._bottomPlaceholder.domElement.innerHTML=`<div class="${styles.app}">
+      <div class="${styles.bottom}">
+        <i class="ms-Icon ms-Icon--Info" aria-hidden="true"></i> bottom
+      </div>
+    </div>`;
+      }
+    }
+  }
+
+  private _onDispose(): void {
+    console.log('[HelloWorldApplicationCustomizer._onDispose] Disposed custom top and bottom placeholders.');
+  }
+}
---
+++ src/extensions/spfxExtension1/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ISpfxExtension1ApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'SpfxExtension1ApplicationCustomizerStrings' {
+  const strings: ISpfxExtension1ApplicationCustomizerStrings;
+  export = strings;
+}
